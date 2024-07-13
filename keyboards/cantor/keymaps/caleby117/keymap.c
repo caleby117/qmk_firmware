c6417baea6840c2f05bba5d694fda6e42f468f3b
caleby <caleby117@gmail.com>
2024-07-13 06:19:39 +0000

    feat(keymap) optimised layer 3

--- keyboards/cantor/keymaps/caleby117/keymap.c
+++ keyboards/cantor/keymaps/caleby117/keymap.c
@@ -25,27 +25,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_ESC,  LCTL_T(KC_SPC), KC_BSPC,  KC_ENT, KC_SPC, OSL(1)
     ),
 
-
     [1] = LAYOUT_split_3x6_3(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_NO,   KC_NO,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOT,
-        KC_LSFT, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                             KC_NO,   S(KC_MINS),   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,      KC_5,                              KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_BSLS,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,  S(KC_LBRC),KC_LBRC,                           KC_RBRC, S(KC_RBRC), KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,  KC_NO,     KC_NO,                             KC_NO,   S(KC_MINS), KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             TO(0),   LCTL_T(KC_SPC), KC_BSPC,  KC_ENT, KC_SPC, TO(2)
     ),
 
+
     [2] = LAYOUT_split_3x6_3(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_NO,   KC_NO,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOT,
-        KC_LSFT, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                             KC_NO,   S(KC_MINS),   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,      KC_5,                              KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_BSLS,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,  S(KC_LBRC),KC_LBRC,                           KC_RBRC, S(KC_RBRC), KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,  KC_NO,     KC_NO,                             KC_NO,   S(KC_MINS), KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             TO(0),   LCTL_T(KC_SPC), KC_BSPC,  KC_ENT, KC_SPC, TO(3)
     ),
 
     [3] = LAYOUT_split_3x6_3(
-        KC_F1,   KC_F2,   KC_F3,   KC_MS_U,   KC_F5,   KC_F6,                              KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_NO,   KC_NO,
-        KC_GRV,  KC_1,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_5,                               KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_NO,   KC_ENT,
-        KC_LSFT, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                             KC_NO,   S(KC_MINS),   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            TO(0),   LCTL_T(KC_SPC), KC_BSPC,  KC_ENT, KC_SPC, TO(2)
-    )
+        KC_NO,  KC_NO,    KC_NO,    KC_MS_U,   KC_NO,      KC_NO,                              KC_NO,    KC_WH_D,       KC_WH_U,    KC_NO,    KC_NO,    KC_NO,
+        KC_NO,   KC_NO,   KC_MS_L,   KC_MS_D,   KC_MS_R,KC_NO,                           KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,  KC_NO, KC_NO,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,  KC_NO,     KC_NO,                             KC_NO,   S(KC_MINS), KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            TO(0),   LCTL_T(KC_SPC), KC_MS_BTN1,  KC_MS_BTN2, KC_SPC, TO(2)
+    ),
+
 
 };
 
